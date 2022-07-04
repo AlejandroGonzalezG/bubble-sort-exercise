c6b265c96ff6645f8c6df601ce0c760ebce41acf
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-04 20:06:00 +0000

    avance mostrar cartas random

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let draw = document.getElementById("draw");
 draw.addEventListener("click", myFunction);
 
 const numerosTotales = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const pintasTotales = ["&spades", "&clubs", "&hearts", "&diams"];
+const pintasTotales = ["♦", "♥", "♠", "♣"];
 
 function Carta(pinta, numero) {
     this.pinta = pinta;
@@ -21,25 +21,30 @@ function myFunction() {
         arrSalida.push(cartaDeEntrada);
     }
     console.log(arrSalida);
+    agregarCartas(arrSalida);
     return arrSalida;
 }
 
 const generadorCartas = (cartaDeEntrada) => {
-    if (["&spades", "&clubs"].includes(cartaDeEntrada.pinta)) {
+    if (["♠", "♣"].includes(cartaDeEntrada.pinta)) {
         return `<div class="card">
         <div class="pinta">${cartaDeEntrada.pinta} </div>
         <div class="numero">${cartaDeEntrada.numero} </div>
         <div class="pintaAbajo">${cartaDeEntrada.pinta} </div>
     </div>`
-    }
+    } else return `<div class="card">
+    <div class="pinta" style="color: red">${cartaDeEntrada.pinta} </div>
+    <div class="numero">${cartaDeEntrada.numero} </div>
+    <div class="pintaAbajo" style="color: red">${cartaDeEntrada.pinta} </div>
+</div>`
 }
 
 const agregarCartas = (arrSalida) => {
     cartasCreadas = "";
     for (let i = 0; i < arrSalida.length; i++) {
         cartasCreadas = cartasCreadas + generadorCartas(arrSalida[i]);
-        let contenedor = document.getElementById("#container");
-        document.contenedor.appendChild(cartasCreadas);  
+        let contenedor = document.getElementById("container");
+        console.log(cartasCreadas);
+        contenedor.innerHTML = cartasCreadas;
     }
 }
-
