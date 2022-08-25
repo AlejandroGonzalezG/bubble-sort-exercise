743d88798621bf9729df230554a1a1a716e60b8b
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-08-25 05:14:15 +0000

    termino de cards y bubble sort

--- app.js
+++ app.js
@@ -1,50 +1,94 @@
-let draw = document.getElementById("draw");
-draw.addEventListener("click", myFunction);
+let botonDraw = document.getElementById("draw");
+let botonSort = document.getElementById("sort");
 
 const numerosTotales = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 const pintasTotales = ["♦", "♥", "♠", "♣"];
 
 function Carta(pinta, numero) {
-    this.pinta = pinta;
-    this.numero = numero;
+  this.pinta = pinta;
+  this.numero = numero;
 };
 
 let arrSalida = [];
 
-function myFunction() {
-    let entrada = document.getElementById("input2").value;
-    let arrSalida = [];
-    for (let i = 1; i <= entrada; i++) {
-        let cartaDeEntrada = new Carta();
-        cartaDeEntrada.numero = numerosTotales[Math.floor((Math.random() * 13))];
-        cartaDeEntrada.pinta = pintasTotales[Math.floor((Math.random() * 4))];
-        arrSalida.push(cartaDeEntrada);
-    }
-    console.log(arrSalida);
-    agregarCartas(arrSalida);
-    return arrSalida;
+const clearSortedContainer = () => {
+  let cardsContainerSorted = document.getElementById("container-sort");
+  while (cardsContainerSorted.firstChild) {
+    cardsContainerSorted.removeChild(cardsContainerSorted.firstChild);
+  }
+};
+
+function myFunction(entrada) {
+  let arrSalida = [];
+  for (let i = 1; i <= entrada; i++) {
+    let cartaDeEntrada = new Carta();
+    cartaDeEntrada.numero = numerosTotales[Math.floor((Math.random() * 13))];
+    cartaDeEntrada.pinta = pintasTotales[Math.floor((Math.random() * 4))];
+    arrSalida.push(cartaDeEntrada);
+  }
+  agregarCartas(arrSalida);
+  return arrSalida;
 }
 
 const generadorCartas = (cartaDeEntrada) => {
-    if (["♠", "♣"].includes(cartaDeEntrada.pinta)) {
-        return `<div class="card">
+  if (["♠", "♣"].includes(cartaDeEntrada.pinta)) {
+    return `<div class="card">
         <div class="pinta">${cartaDeEntrada.pinta} </div>
         <div class="numero">${cartaDeEntrada.numero} </div>
         <div class="pintaAbajo">${cartaDeEntrada.pinta} </div>
     </div>`
-    } else return `<div class="card">
+  } else return `<div class="card">
     <div class="pinta" style="color: red">${cartaDeEntrada.pinta} </div>
     <div class="numero">${cartaDeEntrada.numero} </div>
     <div class="pintaAbajo" style="color: red">${cartaDeEntrada.pinta} </div>
 </div>`
 }
 
-const agregarCartas = (arrSalida) => {
-    cartasCreadas = "";
-    for (let i = 0; i < arrSalida.length; i++) {
-        cartasCreadas = cartasCreadas + generadorCartas(arrSalida[i]);
-        let contenedor = document.getElementById("container");
-        console.log(cartasCreadas);
-        contenedor.innerHTML = cartasCreadas;
+const agregarCartas = (arrSalida, sort, numIterador) => {
+  let cardsContainer = document.getElementById("container");
+  let cardsContainerSorted = document.getElementById("container-sort");
+  let cartasCreadas = "";
+  for (let i = 0; i < arrSalida.length; i++) {
+    cartasCreadas = cartasCreadas + generadorCartas(arrSalida[i]);}
+    let contenedor = document.getElementById("container");
+    contenedor.innerHTML = cartasCreadas;
+    if (!sort) {
+      cardsContainer.innerHTML = cartasCreadas;
+    } else {
+      cardsContainerSorted.innerHTML += '<div class="cards-container-sort-row"> <div class="container-iteration-num"> <p class="iteration-num">' + numIterador + '</p> </div> ' + cartasCreadas + ' </div>';
     }
-}
+  }
+
+
+const bubbleSort = (arrSalida) => {
+  let newArrSalida = [...arrSalida];
+  let numIterador = -1;
+  for (let i = 0; i < newArrSalida.length; i++) {
+    for (let j = 0; j < newArrSalida.length - i - 1; j++) {
+      if (numerosTotales.indexOf(newArrSalida[j].numero) > numerosTotales.indexOf(newArrSalida[j + 1].numero)) {
+        const lesser = newArrSalida[j + 1];
+        newArrSalida[j + 1] = newArrSalida[j];
+        newArrSalida[j] = lesser;
+        numIterador++;
+        agregarCartas(newArrSalida, true, numIterador);
+      }
+    }
+  }
+
+  if (numIterador == -1) {
+    agregarCartas(newArrSalida, true, 0);
+  }
+
+  return newArrSalida;
+};
+
+botonDraw.addEventListener('click', () => {
+  let entrada = document.getElementById("input2").value;
+  arrSalida = myFunction(entrada);
+  agregarCartas(arrSalida, false, 0);
+});
+
+botonSort.addEventListener('click', () => {
+  clearSortedContainer();
+  bubbleSort(arrSalida);
+});
